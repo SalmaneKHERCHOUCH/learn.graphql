bdea755a66d72d5cdfcfb15f851847fbf50d43a5
salmane.info <salmane.kherchouch@github.com>
2021-10-27 09:16:33 +0200

    Ajout de mes modèles

--- src/service.js
+++ src/service.js
@@ -1,2 +1,47 @@
-console.log("Hello boys")
+const {ApolloServer, gql} = require('apollo-server');
 
+const typeDefs = gql `
+
+    type User {
+        id: Int
+        pseudo: String
+        mail: String
+        age: Number
+        numero_portable: String
+    }
+
+    type Post {
+        id: Int
+
+    }
+
+
+    type Mutation {
+
+    }
+`
+const post = [
+    {
+        title : "Premier",
+        id : 1,
+    },
+    {
+        title : "Deuxieme",
+        id : 2,
+    },
+];
+
+const resolvers = {
+    Query: {
+      getPost: () => post,
+    },
+  };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+
+
+;

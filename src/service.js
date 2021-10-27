db2e7a0f95a4348c3d4dc9a0e75209500d5b5a7c
salmane.info <salmane.kherchouch@github.com>
2021-10-27 14:01:02 +0200

    Ajout d'information User

--- src/service.js
+++ src/service.js
@@ -8,33 +8,60 @@ const typeDefs = gql `
         mail: String
         age: Int
         numero_portable: String
+        posts : [Post]
     }
 
     type Post {
         id: Int
         title : String
         content : String
-        author : String
+        author : User
     }
 
     type Query {
         getPost: [Post]
+        getUser: [User]
     }
 `
 const post = [
     {
         id: 1,
-        content: "Premier message"
+        content: "Premier message",
+        author: "Salmane",
+        title: "First"
     },
     {
         id: 2,
-        content: "Deuxieme message"
+        content: "Deuxieme message",
+        author: "Salmane",
+        title: "Second"
+    }
+]
+
+const user = [
+    {
+        id: 1,
+        pseudo: "Darkstone",
+        mail: "dark@hotmail.com",
+        age: 25,
+        numero_portable: "0602030504",
+        posts: post
+
+    },
+    {
+        id: 2,
+        pseudo: "Zornos",
+        mail: "zornos@gmail.com",
+        age: 23,
+        numero_portable: "0789561234",
+        posts: post 
     }
 ]
 
 const resolvers = {
     Query: {
       getPost: () => post,
+      getUser: () => user,
     },
   };
 
